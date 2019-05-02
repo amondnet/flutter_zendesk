908db278f3c818a89e1c388d2003919453675ec5
Minsu Lee <amond@amond.net>
2019-05-02 15:03:49 +0900

    remove unused

--- example/lib/test_main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_zendesk_example/main.dart' as app;
-
-void main() {
-  app.main([
-    'd4ffe290c8c9f234c52cd80b525cce1e747a2ccf32833f23',
-    'mobile_sdk_client_3e6256e77123f7c6b125',
-    'https://chatbotpf.zendesk.com'
-  ]);
-}
