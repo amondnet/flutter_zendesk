45477a0b3a8b04118162b721b8df94a5ddd65371
Minsu Lee <amond@amond.net>
2019-05-08 16:20:35 +0900

    export

---
+++ lib/model.dart
@@ -0,0 +1,6 @@
+export 'src/model/article.dart';
+export 'src/model/comment.dart';
+export 'src/model/request.dart';
+export 'src/model/requests_with_commenting_agents.dart';
+export 'src/model/comment_with_user.dart';
+export 'src/model/support_user.dart';
